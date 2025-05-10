e20173a4ee47dce66b9aa670f7728e8f953114f0
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-05-10 03:54:18 +0300

    Test commit

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fqdmnoixa@09hb2$8@5x6@a0+fyttp8$pv)-0=6=nt)$areg#4'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') # django-insecure-fqdmnoixa@09hb2$8@5x6@a0+fyttp8$pv)-0=6=nt)$areg#4
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
